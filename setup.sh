ff4e53fb5886f06fbb46e65c285cf05ae31287c7
William Rose <william@picnichealth.com>
2022-10-10 09:27:28 -0700

    Add VSCode launch config

--- setup.sh
+++ setup.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
-
 DOTFILES="$HOME/.dotfiles"
 
-# Set up our .zshrc
+function log {
+  echo "[$(date +'%F %T')] $*"
+}
+
+# Set up our .zshrc, replacing if it exists.
+log "Copying .zshrc"
 cp -f "$DOTFILES/zshrc" "$HOME/.zshrc"
 
-# Put our docker-compose.override.yml into the workspace
-if test -d "$GITPOD_REPO_ROOT" -a \! -f "$GITPOD_REPO_ROOT/docker-compose.override.yml"
+if ! test -d "$GITPOD_REPO_ROOT"
 then
-  cp "$DOTFILES/docker-compose.override.yml" "$GITPOD_REPO_ROOT/docker-compose.override.yml"
+  >&2 echo "Workspace root value $GITPOD_REPO_ROOT does not refer to a directory"
+  exit 0
 fi
+
+# Copy launch.json if not present
+log "Copying launch.json (if not present)"
+test -d "$GITPOD_REPO_ROOT/.vscode" ||
+  mkdir -p "$GITPOD_REPO_ROOT/.vscode"
+test -f "$GITPOD_REPO_ROOT/.vscode/launch.json" ||
+  cp -v "$DOTFILES/vscode-launch.jsonc" "$GITPOD_REPO_ROOT/.vscode/launch.json"
+
+# Copy docker-compose.override.yml if not present
+log "Copying docker-compose.override.yml (if not present)"
+test -f "$GITPOD_REPO_ROOT/docker-compose.override.yml" ||
+  cp -v "$DOTFILES/docker-compose.override.yml" "$GITPOD_REPO_ROOT/docker-compose.override.yml"
+
+log "Done"
