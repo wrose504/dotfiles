0a921ddaa965989bb3937cfaf404a37b1357b55c
william <william@picnichealth.com>
2024-02-05 19:59:35 +0000

    Update for coder

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DOTFILES="$HOME/.dotfiles"
+cd "$(dirname "$BASH_SOURCE")"
 
 function log {
   echo "[$(date +'%F %T')] $*"
@@ -7,24 +7,20 @@ function log {
 
 # Set up our .zshrc, replacing if it exists.
 log "Copying .zshrc"
-cp -f "$DOTFILES/zshrc" "$HOME/.zshrc"
+cp -f "./zshrc" "$HOME/.zshrc"
 
-if ! test -d "$GITPOD_REPO_ROOT"
+# Try to find the checked out repo root
+# In gitpod we have an environment variable. In coder, it's just ~/picnic.
+WORKSPACE="${GITPOD_REPO_ROOT:-$HOME/picnic}"
+if ! test -d "$WORKSPACE"
 then
-  >&2 echo "Workspace root value $GITPOD_REPO_ROOT does not refer to a directory"
+  >&2 echo "Workspace root value $WORKSPACE does not refer to a directory"
   exit 0
 fi
 
-# Copy launch.json if not present
-log "Copying launch.json (if not present)"
-test -d "$GITPOD_REPO_ROOT/.vscode" ||
-  mkdir -p "$GITPOD_REPO_ROOT/.vscode"
-test -f "$GITPOD_REPO_ROOT/.vscode/launch.json" ||
-  cp -v "$DOTFILES/vscode-launch.jsonc" "$GITPOD_REPO_ROOT/.vscode/launch.json"
-
 # Copy docker-compose.override.yml if not present
 log "Copying docker-compose.override.yml (if not present)"
-test -f "$GITPOD_REPO_ROOT/docker-compose.override.yml" ||
-  cp -v "$DOTFILES/docker-compose.override.yml" "$GITPOD_REPO_ROOT/docker-compose.override.yml"
+test -f "$WORKSPACE/docker-compose.override.yml" ||
+  cp -v "./docker-compose.override.yml" "$WORKSPACE/docker-compose.override.yml"
 
 log "Done"
